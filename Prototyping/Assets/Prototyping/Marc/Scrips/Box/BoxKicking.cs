3083f39c875f001aa21650cf8f638e0e912edc9b
Boxter5000 <marc.schuster60@web.de>
2021-02-02 15:34:04 +0100

    Bug Fixing and commenting

--- Prototyping/Assets/Prototyping/Marc/Scrips/Box/BoxKicking.cs
+++ Prototyping/Assets/Prototyping/Marc/Scrips/Box/BoxKicking.cs
@@ -7,52 +7,68 @@ public class BoxKicking : MonoBehaviour
     public float moveSpeed = 5;
     public Transform movePoint;
 
+
     public LayerMask whatStopsMovement;
-    private Vector2 lastBoxPos;
+    public List<Vector3> LastBoxMoves;
+    
 
     void Start()
     {
         movePoint.parent = null;
-        lastBoxPos = transform.position;
+        LastBoxMoves = new List<Vector3>();
     }
 
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("Vertical") || Input.GetButtonDown("Horizontal"))
+        {
+            UpdateBoxPosition();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReturnToLastPosition();
+        }
     }
 
     public void MoveBoxHorizontal(float direktion)
     {
-        lastBoxPos = movePoint.position;
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
             if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(direktion, 0f, 0f), .2f, whatStopsMovement))
             {
-                lastBoxPos = movePoint.position;
                 movePoint.position += new Vector3(direktion, 0f, 0f);
             }
         }
     }
     public void MoveBoxVertical(float direktion)
     {
-        lastBoxPos = movePoint.position;
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
             if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, direktion, 0f), .2f, whatStopsMovement))
             {
-                lastBoxPos = movePoint.position;
                 movePoint.position += new Vector3(0f, direktion, 0f);
             }
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    /*private void OnCollisionEnter2D(Collision2D collision)
     {
-        movePoint.position = lastBoxPos;
-    }
+        Debug.Log("Collidet");
+        if(collision.gameObject.tag == "Enemy")
+        {
+        movePoint.position = LastBoxMoves[LastBoxMoves.Count - 1];
+        }
+    }*/
 
     public void ReturnToLastPosition()
     {
-        movePoint.position = lastBoxPos;
+        movePoint.position = LastBoxMoves[LastBoxMoves.Count - 2];
+        LastBoxMoves.RemoveAt(LastBoxMoves.Count - 1);
+    }
+
+    public void UpdateBoxPosition()
+    {
+        LastBoxMoves.Add(movePoint.position);
     }
 }
--- Prototyping/Assets/Prototyping/Marc/Scrips/Player/GridbasedPlayerController.cs
+++ Prototyping/Assets/Prototyping/Marc/Scrips/Player/GridbasedPlayerController.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GridbasedPlayerController : MonoBehaviour
 {
+    //Parameter
     public float moveSpeed = 5;
     public Transform movePoint;
 
-    [SerializeField]public LayerMask whatStopsMovement;
-    [SerializeField]public LayerMask whatIsPushable;
+    public LayerMask whatStopsMovement;
+    public LayerMask whatIsPushable;
 
-    public List<Vector3> LastMoves;
+    public List<Vector3> LastPlayerMoves;
     float RayDistance = 1f;
     Vector2 lastPlayerPos;
     Vector2 movement;
@@ -20,8 +22,9 @@ public class GridbasedPlayerController : MonoBehaviour
 
     void Start()
     {
+        // split movepoint from Player / create list to remember moves
         movePoint.parent = null;
-        LastMoves = new List<Vector3>(30);
+        LastPlayerMoves = new List<Vector3>();
     }
 
     void Update()
@@ -29,17 +32,18 @@ public class GridbasedPlayerController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        //if any move button is pressed, remember the last direktion for the Raycast
         if (movement.x != 0f || movement.y != 0f)
         {
             LastDirektion = movement;
             MovePlayer();
         }
-
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             ReturnOneMove();
         }
 
+        //try to move to the Movepoint
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
     }
 
@@ -47,13 +51,18 @@ public class GridbasedPlayerController : MonoBehaviour
     {
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
+            // Horizontal movement
             if (Mathf.Abs(movement.x) == 1f)
             {
+                //check if the path is free to move frome anything that shood stop your movement
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(movement.x, 0f, 0f), .2f, whatStopsMovement))
                 {
-                    LastMoves.Add(movePoint.position);
+                    //save the move
+                    SaveMove();
                     movePoint.position += new Vector3(movement.x, 0f, 0f);
                 }
+
+                //if the circle is detektng something that can be pushed, chast a ray to know what object it is
                 if (Physics2D.OverlapCircle(movePoint.position + new Vector3(movement.x, 0f, 0f), .2f, whatIsPushable))
                 {
                     RaycastHit2D hit = Physics2D.Raycast(transform.position, LastDirektion * transform.localScale.x, RayDistance, whatIsPushable);
@@ -61,6 +70,11 @@ public class GridbasedPlayerController : MonoBehaviour
                     {
                         box = hit.collider.gameObject;
                         box.GetComponent<BoxKicking>().MoveBoxHorizontal(movement.x);
+                        //save the move
+                        if (Input.GetButtonDown("Horizontal"))
+                        {
+                            SaveMove();
+                        }
                     }
                     else
                     {
@@ -68,20 +82,26 @@ public class GridbasedPlayerController : MonoBehaviour
                     }
                 }
             }
+            // Vertical movement, Same as horizontal, just in y dir.
             else if (Mathf.Abs(movement.y) == 1f)
             {
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, movement.y, 0f), .2f, whatStopsMovement))
                 {
-                    LastMoves.Add(movePoint.position);
+                    SaveMove();
                     movePoint.position += new Vector3(0f, movement.y, 0f);
                 }
                 if (Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, movement.y, 0f), .2f, whatIsPushable))
                 {
+
                     RaycastHit2D hit = Physics2D.Raycast(transform.position, LastDirektion * transform.localScale.x, RayDistance, whatIsPushable);
                     if (hit)
                     {
                         box = hit.collider.gameObject;
                         box.GetComponent<BoxKicking>().MoveBoxVertical(movement.y);
+                        if (Input.GetButtonDown("Vertical"))
+                        {
+                            SaveMove();
+                        }
                     }
                     else
                     {
@@ -94,16 +114,30 @@ public class GridbasedPlayerController : MonoBehaviour
 
     private void ReturnOneMove()
     {
-        if(LastMoves.Count > 0)
+        if(LastPlayerMoves.Count > 0)
         {
-        movePoint.position = LastMoves[LastMoves.Count - 1];
-        LastMoves.RemoveAt(LastMoves.Count - 1);
-        FindObjectOfType<BoxKicking>().ReturnToLastPosition();
+        movePoint.position = LastPlayerMoves[LastPlayerMoves.Count - 1];
+        LastPlayerMoves.RemoveAt(LastPlayerMoves.Count - 1);
         }
         else
         {
             Debug.LogWarning("No More Moves Left to go back");
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("aua");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+
+    public void SaveMove()
+    {
+        LastPlayerMoves.Add(movePoint.position);
     }
 
     private void OnDrawGizmos()
