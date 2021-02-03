762edb9095379c2da6b0331211fd3b0374bcd897
Boxter5000 <marc.schuster60@web.de>
2021-02-03 17:51:39 +0100

    implement lvl1 to main sceen, slimes also return moves

---
+++ Prototyping/Assets/Prototyping/Marc/Scrips/enemys/Slime/SlimeMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public Transform movePoint;
+    public float MoveDirektionX;               // 1 if you the Enemy to move Horitontal 
+    public float MoveDirektionY;                 // 1 if you want the enemy to move vertical
+    public float moves;
+    public LayerMask whatStopsMovement;
+
+    float movesLeft;
+    Vector2 movement;
+    Vector2 LastDirektion;
+    public List<Vector3> LastEnemyMove;
+
+    void Start()
+    {
+        movePoint.parent = null;
+        movesLeft = moves;
+
+        LastEnemyMove = new List<Vector3>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x != 0f || movement.y != 0f)
+        {
+            LastDirektion = movement;
+            MoveSlime();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReturnToLastPosition();
+        }
+    }
+
+    public void MoveSlime()
+    {
+        if (LastDirektion.x != 0f || LastDirektion.y != 0f)
+        {
+            if (Vector3.Distance(transform.position, movePoint.position) <= 0f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(MoveDirektionX, MoveDirektionY, 0f), .2f, whatStopsMovement))
+                {
+                    LastDirektion.x = 0f;
+                    LastDirektion.y = 0f;
+
+                    movePoint.position += new Vector3(MoveDirektionX, MoveDirektionY, 0f);
+                    movesLeft--;
+                    SaveMove();
+                    if (movesLeft <= 0)
+                    {
+                    MoveDirektionX *= -1;
+                    MoveDirektionY *= -1;
+                        movesLeft = moves;
+                    }
+                }
+            }
+        }
+    }
+    public void SaveMove()
+    {
+        LastEnemyMove.Add(movePoint.position);
+    }
+
+    public void ReturnToLastPosition()
+    {
+        movePoint.position = LastEnemyMove[LastEnemyMove.Count - 2];
+        LastEnemyMove.RemoveAt(LastEnemyMove.Count - 1);
+        movesLeft++;
+        if(movesLeft == moves)
+        {
+            movesLeft = 0f;
+        }
+    }
+}
